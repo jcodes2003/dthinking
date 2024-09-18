8c25aa9d78ec7cf724367fff32d632f52fb79153
jcodes2003 <calmaj2003@gmail.com>
2024-09-18 23:24:29 +0800

    meow meow

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,7 @@ class _LoginPageState extends State<LoginPage> {
               context: context,
               builder: (BuildContext context) {
                 return AlertDialog(
-                  title: const Text('Login hays'),
+                  title: const Text('Login ha'),
                   content: Text(result['message']),
                   actions: <Widget>[
                     TextButton(
